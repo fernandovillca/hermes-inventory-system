dd5ece0cb5f4fccb95e855a443e2fb03f941a13d
Fernando <vluisfernando62@gmail.com>
2025-09-30 17:05:44 -0400

    feat(dashboard): implement dynamic breadcrumbs in admin dashboard layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,17 @@
-<x-admin-layout>
+@php
+    $breadcrumbs = [
+        ['name' => 'Dashboard', 'href' => route('admin.dashboard')],
+        ['name' => 'Pruebas']
+    ];
+@endphp
+
+<x-admin-layout 
+    title="Dashboard - Hermes System"
+    :breadcrumbs="$breadcrumbs"
+>
+
+    <x-slot name="action">
+        hola mundoo
+    </x-slot>
     Hola desde el dashboard
 </x-admin-layout>
